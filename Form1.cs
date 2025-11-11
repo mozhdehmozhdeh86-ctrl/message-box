3619a2ae9e6a44465866aebed801bef797a37e95
mozhdehmozhdeh86-ctrl <mozhdehmozhdeh86@gmail.com>
2025-11-11 17:57:50 +0330

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -8,22 +8,229 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication18
+namespace Side_Scrolling_Game_MOO_ICT
 {
     public partial class Form1 : Form
     {
+
+
+        bool goLeft, goRight, jumping, hasKey;
+
+        int jumpSpeed = 30;
+        int force = 20;
+        int score = 0;
+
+        int playerSpeed = 10;
+        int backgroundSpeed = 8;
+
+
+
         public Form1()
         {
             InitializeComponent();
+            //txtScore.Parent = background;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void MainTimerEvent(object sender, EventArgs e)
         {
-            DialogResult result = MessageBox.Show("آیا از خروج خود اطمینان دارید؟", "warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-            if(result==DialogResult.Yes)
+            txtScore.Text = "Score: " + score;
+            txtScore.Left = 536;
+            player.Top += jumpSpeed;
+
+            if (goLeft == true && player.Left > 60)
             {
-                Application.Exit();
+                player.Left -= playerSpeed;
+            }
+            if (goRight == true && player.Left + (player.Width + 60) < this.ClientSize.Width )
+            {
+                player.Left += playerSpeed;
+            }
+
+
+            if (goLeft == true && background.Left < 0)
+            {
+                background.Left += backgroundSpeed;
+                MoveGameElements("forward");
+            }
+
+            if (goRight == true && background.Left > -1372)
+            {
+                background.Left -= backgroundSpeed;
+                MoveGameElements("back");
+            }
+
+            if (jumping == true)
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+
+            if (jumping == true && force < 0)
+            {
+                jumping = false;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+
+                if (x is PictureBox && (string)x.Tag == "platform")
+                {
+
+                    if (player.Bounds.IntersectsWith(x.Bounds) && jumping == false)
+                    {
+                        force = 10;
+                        player.Top = x.Top - player.Height;
+                        jumpSpeed = 0;
+                    }
+
+                    x.BringToFront();
+
+                }
+                if (x is PictureBox && (string)x.Tag == "coin")
+                {
+
+                    if (player.Bounds.IntersectsWith(x.Bounds) && x.Visible == true)
+                    {
+                        x.Visible = false;
+                        score += 1;
+                    }
+
+                }
+            }
+
+            if (player.Bounds.IntersectsWith(key.Bounds))
+            {
+                key.Visible = false;
+                hasKey = true;
+            }
+
+            if (player.Bounds.IntersectsWith(door.Bounds) && hasKey == true)
+            {
+                door.Image = Properties.Resources.door_open;
+                GameTimer.Stop();
+                MessageBox.Show("تبریک میگم,شما برنده شدید ! " + Environment.NewLine + "Click OK to play again");
+                RestartGame();
+            }
+
+            if (player.Top + player.Height > this.ClientSize.Height)
+            {
+                GameTimer.Stop(); 
+                MessageBox.Show("شما باختید!" + Environment.NewLine + "Click OK to play again");
+                RestartGame();
             }
         }
+
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Space && jumping == false)
+            {
+                jumping = true;
+            }
+
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (jumping == true)
+            {
+                jumping = false;
+            }
+        }
+
+        private void CloseGame(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void RestartGame()
+        {
+            Form1 newWindow = new Form1();
+            newWindow.Show();
+            this.Hide();
+        }
+
+        private void MoveGameElements(string direction)
+        {
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "platform" || x is PictureBox && (string)x.Tag == "coin" || x is PictureBox && (string)x.Tag == "key" || x is PictureBox && (string)x.Tag == "door")
+                {
+
+                    if (direction == "back")
+                    {
+                        x.Left -= backgroundSpeed;
+                    }
+                    if (direction == "forward")
+                    {
+                        x.Left += backgroundSpeed;
+                    }
+
+
+                }
+            }
+
+
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void background_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox15_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
